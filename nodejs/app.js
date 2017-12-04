a328272fbe275825fa90aea88e194c20519ed95e
Merve Erdol <merve-uygun@windowslive.com>
2017-12-04 14:03:07 +0300

    Get input from the command line

--- nodejs/app.js
+++ nodejs/app.js
@@ -4,3 +4,18 @@ const fs = require('fs'); // fetch all contents of fs module and store them in f
 const _ = require('lodash');
 
 const notes = require('./notes');
+
+const command = process.argv[2]; //grab command line info
+console.log('Command;', command);
+
+if (command === 'add') {
+  console.log('Adding a new note');
+} else if (command === 'list') {
+  console.log('Listing all notes');
+} else if (command === 'remove') {
+  console.log('Note is removed');
+} else if (command === 'read') {
+  console.log('Readig note');
+} else {
+  console.log('Command not recognized');
+}
