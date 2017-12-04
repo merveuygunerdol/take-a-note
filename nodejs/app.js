92165ef343a5433d5d1c42d6368e665abfae3738
Merve Erdol <merve-uygun@windowslive.com>
2017-12-04 13:36:23 +0300

    Require fs module & add notes.js

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,3 +1,6 @@
 console.log('Starting app');
 
+const fs = require('fs'); // fetch all contents of fs module and store them in fs var // we can access all functions of fs
 const _ = require('lodash');
+
+const notes = require('./notes');
