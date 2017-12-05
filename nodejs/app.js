73779df2345e21f9b5e03992803307ce2f99d601
Merve Erdol <merve-uygun@windowslive.com>
2017-12-05 13:16:14 +0300

    Simplify inputs with yargs

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,20 +2,23 @@ console.log('Starting app');
 
 const fs = require('fs'); // fetch all contents of fs module and store them in fs var // we can access all functions of fs
 const _ = require('lodash');
+const yargs = require('yargs'); // parsing arguments
 
 const notes = require('./notes');
 
-const command = process.argv[2]; //grab command line info
+const argv = yargs.argv;
+const command = argv._[0]; // grab command line info
 console.log('Command;', command);
+console.log('Yargs;', argv);
 
 if (command === 'add') {
-  console.log('Adding a new note');
+  notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-  console.log('Listing all notes');
+  notes.getAll();
 } else if (command === 'remove') {
-  console.log('Note is removed');
+  notes.removeNote(argv.title);
 } else if (command === 'read') {
-  console.log('Readig note');
+  notes.readNote(argv.title, argv.body);
 } else {
   console.log('Command not recognized');
 }
--- nodejs/notes.js
+++ nodejs/notes.js
@@ -1,1 +1,24 @@
 console.log('Starting notes.js');
+
+const addNote = (title, body) => {
+  console.log('Adding note: Title:', title, 'Body:', body);
+};
+
+const getAll = () => {
+  console.log('Listing all notes');
+};
+
+const removeNote = (title) => {
+  console.log('Removing the note: Title:', title);
+};
+
+const readNote = (title, body) => {
+  console.log('Reading the note: Title:', title, 'Body:', body);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  removeNote,
+  readNote,
+};
